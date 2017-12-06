2310459a6e42789c1622f45b6f4069fb3e9bd5ff
Mary Mazi <mazi.mary.o@gmail.com>
2017-12-06 17:28:53 +0100

    Create postgres container to persist vote data

--- src/app.js
+++ src/app.js
@@ -10,6 +10,20 @@ let votes = {
   sublimeText: 0
 };
 
+let client = new pg.Client('postgres://postgres@172.17.0.1:9000/postgres');
+
+client.connect((error) => {
+   if (error) throw error;
+
+   client.query('SELECT number_of_votes FROM votes', (error, result) => {
+    if (error) throw error;
+    
+    votes.visualStudio = result.rows[0].number_of_votes;
+    votes.sublimeText  = result.rows[1].number_of_votes;
+   })
+})
+
+
 // view engine setup
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +40,14 @@ app.post('/vote', (req, res) => {
   let vote = req.body.yourVote;
   if (vote === 'visualStudio') {
     votes.visualStudio = votes.visualStudio + 1;
+    client.query(`UPDATE votes SET number_of_votes=${votes.visualStudio} WHERE option_name='visualStudio'`, (error, result) => {
+      if (error) throw error;
+    })
   } else if (vote === 'sublimeText') {
     votes.sublimeText = votes.sublimeText + 1;
+    client.query(`UPDATE votes SET number_of_votes=${votes.sublimeText} WHERE option_name='sublimeText'`, (error, result) => {
+      if (error) throw error;
+    })
   } else {
     console.log(`Something went wrong: vote contains  ${vote}`);
   }
